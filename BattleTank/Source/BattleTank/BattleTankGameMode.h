c9661e55281313f51259a85dfb1eb55490f2af56
Christian Lett <christian@vfxsoup.uk>
2017-08-24 14:28:21 +0100

    04_113 - 3rd Person Camera Control

--- BattleTank/Source/BattleTank/BattleTankGameMode.h
+++ BattleTank/Source/BattleTank/BattleTankGameMode.h
@@ -4,13 +4,13 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/GameModeBase.h"
-#include "BattleTankGameModeBase.generated.h"
+#include "BattleTankGameMode.generated.h"
 
 /**
  * 
  */
 UCLASS()
-class BATTLETANK_API ABattleTankGameModeBase : public AGameModeBase
+class BATTLETANK_API ABattleTankGameMode : public AGameModeBase
 {
 	GENERATED_BODY()
 	
