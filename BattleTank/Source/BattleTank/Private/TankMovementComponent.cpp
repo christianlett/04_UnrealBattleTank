636bfc65646f73016bddbc2211196b369885f873
Christian Lett <christian@vfxsoup.uk>
2017-09-25 14:06:33 +0100

    04_165 - Finalising Your Class Code AI tanks now move

--- BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
+++ BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
@@ -24,5 +24,14 @@ void UTankMovementComponent::IntendTurnRight(float Throw) {
 
 void UTankMovementComponent::RequestDirectMove(const FVector & MoveVelocity, bool bForceMaxSpeed)
 {
-	UE_LOG(LogTemp, Warning, TEXT("%s is moving with velocity %s"), *GetOwner()->GetName(), *MoveVelocity.ToString());
+	auto TankForward = GetOwner()->GetActorForwardVector().GetSafeNormal();
+	auto AIForwardIntention = MoveVelocity.GetSafeNormal();
+
+	auto ForwardThrow = FVector::DotProduct(TankForward, AIForwardIntention);
+	IntendMoveForward(ForwardThrow);
+
+	auto RightThrow = FVector::CrossProduct(TankForward, AIForwardIntention).Z;
+	IntendTurnRight(RightThrow);
+	
+//	UE_LOG(LogTemp, Warning, TEXT("%s is moving with velocity %s"), *GetOwner()->GetName(), *MoveVelocity.GetSafeNormal().ToString());
 }
